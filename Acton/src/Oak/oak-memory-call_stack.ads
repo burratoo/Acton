8f572e4814ca0ae7ec6d7630531110f2780b8114
S-1-5-21-4209371911-151716228-1378299450-1131 <pat@burratoo.com>
2011-07-17 21:41:53 +1000

    Changeset: 48 Added Call_Stack_Size subtype to support GCC for Acton.

--- Acton/src/Oak/oak-memory-call_stack.ads
+++ Acton/src/Oak/oak-memory-call_stack.ads
@@ -19,14 +19,16 @@ package Oak.Memory.Call_Stack is
       Top, Bottom, Pointer : System.Address := Null_Address;
    end record;
 
-   function Default_Stack_Size return
-     System.Storage_Elements.Storage_Count renames
-     Oak.Processor_Support_Package.Call_Stack.Default_Call_Stack_Size;
+   function Default_Stack_Size return  System.Storage_Elements.Storage_Count
+      renames Oak.Processor_Support_Package.Call_Stack.Default_Call_Stack_Size;
 
    --  Value used to indicate tha no size has been set
 
    Unspecified_Call_Stack_Size : constant Storage_Count := 0;
 
+   subtype Call_Stack_Size is Storage_Elements.Storage_Count range
+     PSP_Stack.Minimum_Call_Stack_Size ..
+      Storage_Elements.Storage_Count'Last;
 private
    --  Warning! This is not a real variable. It is defined in the linker script
    --  and as such does not have any data storage allocated for it. Instead
