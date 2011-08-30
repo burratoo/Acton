c9f618afbdad8178e4e504992909a4294b310077
S-1-5-21-4209371911-151716228-1378299450-1131 <pat@burratoo.com>
2011-08-30 17:21:06 +1000

    Changeset: 161 Oak.Scheduler.Run_The_Bloody_Scheduler_Agent_That_Wanted_To_Be_Woken: Fixed procedure so it now checks  if the current task is above the priority range of the next scheduler agent and also check to make sure the agent pointer is not null.

--- Acton/src/Oak/oak-scheduler.adb
+++ Acton/src/Oak/oak-scheduler.adb
@@ -150,9 +150,9 @@ package body Oak.Scheduler is
                   Reason            => Select_Next_Task);
          end if;
          Agent := SA_Ops.Get_Next_Agent (Agent);
-         exit when Current_Task /= null and then
+         exit when (Current_Task /= null and Agent /= null) and then
            Get_Normal_Priority (Current_Task)
-               < SA_Ops.Get_Highest_Priority (Agent);
+               < SA_Ops.Get_Lowest_Priority (Agent);
       end loop;
       if Chosen_Task = null then
          Chosen_Task := Current_Task;
