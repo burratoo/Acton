6ab53c2cc734f57f23c56d3d32945bd154cd4e22
S-1-5-21-4209371911-151716228-1378299450-1131 <pat@burratoo.com>
2011-09-06 21:40:32 +1000

    Changeset: 192 Turned off support for preallocated stacks. Resolved mistake that assumed the stack storage size was in bits not bytes.

--- Acton/src/GNARL/system.ads
+++ Acton/src/GNARL/system.ads
@@ -153,7 +153,7 @@ private
    Frontend_Layout           : constant Boolean := False;
    Machine_Overflows         : constant Boolean := False;
    Machine_Rounds            : constant Boolean := True;
-   Preallocated_Stacks       : constant Boolean := True;
+   Preallocated_Stacks       : constant Boolean := False;
    Signed_Zeros              : constant Boolean := True;
    Stack_Check_Default       : constant Boolean := False;
    Stack_Check_Probes        : constant Boolean := False;
